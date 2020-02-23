1919784053b919ccda4595e87d54e59554a85558
zengwangfa <zengwangfa@outlook.com>
2020-02-23 19:56:58 +0800

    :white_check_mark: Add: general makefile

---
+++ demo/04.general_makefile/1.test_makefile/a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "a.h"
+
+
+int main(void)
+{
+	printf("hello world\n");
+	printf("A = %d\n", A);
+	test_fun();
+	return 0;
+}
---
+++ demo/04.general_makefile/1.test_makefile/b.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+
+int test_fun(void)
+{
+	printf("it is B\n");
+	return 0;
+}
